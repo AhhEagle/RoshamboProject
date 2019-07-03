53db1c9668f9479ec688bbac9db9216146e55e78
Dimeji <aliyu.olateju@gmail.com>
2019-07-03 23:43:01 +0100

    Implemented result view for players to see the result of each choices made

--- RoshamboProject/PickerViewController.swift
+++ RoshamboProject/PickerViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class PickerViewController: UIViewController {
 
-    @IBOutlet weak var buttonPaper: UIImageView!
-    @IBOutlet weak var buttonRock: UIImageView!
-    @IBOutlet weak var buttonScissors: UIImageView!
+    @IBOutlet weak var buttonPaper: UIButton!
+    @IBOutlet weak var buttonRock: UIButton!
+    @IBOutlet weak var buttonScissors: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func selectPaper(_ sender: UIButton) {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+        controller.userChoice = getShape(sender)
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func selectRock(_ sender: Any) {
+        performSegue(withIdentifier: "select", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "select"{
+            let controller = segue.destination as! ResultViewController
+            controller.userChoice = getShape(sender as! UIButton)
+        }
+    }
+    
+    // The enum "Shape" represents a play or move
+    private func getShape(_ sender: UIButton) -> Picture {
+        // Titles are set to one of: Rock, Paper, or Scissors
+        let shape = sender.title(for: UIControl.State())!
+        return Picture(rawValue: shape)!
+    }
 }
+
 
--- RoshamboProject/ResultViewController.swift
+++ RoshamboProject/ResultViewController.swift
@@ -8,9 +8,63 @@
 
 import UIKit
 
+// The enum "Shape" represents a play or move
+enum Picture: String {
+    case Rock = "Rock"
+    case Paper = "Paper"
+    case Scissors = "Scissors"
+    
+    // This function randomly generates an opponent's play
+    static func randomShape() -> Picture {
+        let shapes = ["Rock", "Paper", "Scissors"]
+        let randomChoice = Int(arc4random_uniform(3))
+        return Picture(rawValue: shapes[randomChoice])!
+    }
+}
+
 class ResultViewController: UIViewController {
     
     @IBOutlet weak var resultButton: UILabel!
     @IBOutlet weak var reset: UIButton!
     @IBOutlet weak var resultImage: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        displayResult()
+    }
+    
+    // When the ResultViewController is initialized a userChoice is passed in and an opponent's play is generated.
+    var userChoice: Picture!
+    private let systemChoice: Picture = Picture.randomShape()
+    
+    
+    private func displayResult() {
+        // Ideally, most of this would be handled by a model.
+        var imageName: String
+        var text: String
+        let matchup = "\(userChoice.rawValue) vs. \(systemChoice.rawValue)"
+        
+        // Why is an exclamation point necessary? :)
+        switch (userChoice!, systemChoice) {
+        case let (user, opponent) where user == opponent:
+            text = "\(matchup): it's a tie!"
+            imageName = "tie"
+        case (.Rock, .Scissors), (.Paper, .Rock), (.Scissors, .Paper):
+            text = "You win with \(matchup)!"
+            imageName = "\(userChoice.rawValue)-\(systemChoice.rawValue)"
+        default:
+            text = "You lose with \(matchup) :(."
+            imageName = "\(systemChoice.rawValue)-\(userChoice.rawValue)"
+        }
+        
+        imageName = imageName.lowercased()
+        resultImage.image = UIImage(named: imageName)
+        resultButton.text = text
+    }
+    
+   
+    
+    @IBAction func playAgain(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
