0fe27cf45a6ba24e6dc3906df12b9f19d73a0b9f
Dimeji <aliyu.olateju@gmail.com>
2019-07-05 07:43:42 +0100

    Modifications made to the PickerController to allow the new models handle the moves

--- RoshamboProject/PickerViewController.swift
+++ RoshamboProject/PickerViewController.swift
@@ -12,13 +12,57 @@ class PickerViewController: UIViewController {
 
     @IBOutlet weak var buttonPaper: UIButton!
     @IBOutlet weak var buttonRock: UIButton!
-    @IBOutlet weak var buttonScissors: UIButton!
+    @IBOutlet weak var buttonScissor: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func makeMove(_ sender: UIButton) {
+        
+        // Here the RPS enum holds a player's move
+        switch (sender) {
+        case self.buttonPaper:
+            play(RoshamboModel.paper)
+            
+        case self.buttonRock:
+            play(RoshamboModel.rock)
+            
+        case self.buttonScissor:
+            play(RoshamboModel.scissor)
+        default:
+            assert(false, "Invalid")
+            
+        }
+    }
+    
+    //Player and Computer makes a move
+    
+    func play(_ playerMove: RoshamboModel)
+    {
+        // Computer makes a move
+        let computersMove = RoshamboModel()
+        
+        // results of a match is stored
+        self.match = ResultHistory(p1: playerMove, p2: computersMove)
+        
+        
+        if (playerMove == RoshamboModel.paper) {
+            // Get the storyboard and ResultViewController
+            let storyboard = UIStoryboard (name: "PickerViewController", bundle: nil)
+            let controller = storyboard.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+            
+            // the result is passed accross
+            controller.match = self.match
+            self.present(controller, animated: true, completion: nil)
+        }
+            
+        else if (playerMove == RoshamboModel.rock) {
+            performSegue(withIdentifier: "select", sender: self)
+        }
+    }
+    /*
     //Instantiating View using code only
     @IBAction func selectPaper(_ sender: UIButton) {
         let controller = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
@@ -45,7 +89,7 @@ class PickerViewController: UIViewController {
         // Titles are set to one of: Rock, Paper, or Scissors
         let shape = sender.title(for: UIControl.State())!
         return Picture(rawValue: shape)!
-    }
+    }*/
 }
 
 
